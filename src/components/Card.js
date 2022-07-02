07e0eaed49144980f4bacac58508d7573b8550f7
Asmit2952 <asmitbm2952002@gmail.com>
2022-07-02 10:59:36 +0530

    location details

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+
 export default function Card () {
     return (
-        <div>
-            <h1>Card</h1>
+        <div className="entries">
+            <div className="card--image">
+                <img src="https://source.unsplash.com/WLxQvbMyfas" alt="location images"/>
+            </div>
+            <div className="details">
+                <ul className="location">
+                    <li><FontAwesomeIcon icon={faLocationDot} className="location--pin"/></li>
+                    <li><p>Japan</p></li>
+                    <li><a href="https://github.com/Asmit2952" rel="noopener noreferrer" target="_blank" >View on Google Maps</a></li>
+                </ul>
+            </div>
         </div>
     )
 }
